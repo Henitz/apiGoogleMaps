b5d3db62164ef1ab6539710f1c6e75b17e7d342e
Henitz <henitz@gmail.com>
2023-02-19 13:50:35 -0300

    second commit

---
+++ src/main/java/com/googleapigeocode/GoogleapigeocodeApplication.java
@@ -0,0 +1,13 @@
+package com.googleapigeocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleapigeocodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleapigeocodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/googleapigeocode/GoogleapigeocodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googleapigeocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleapigeocodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
